2d2f633e1a689219e41ec008039ae5aee0c33ae5
Charles Ferreira <charles.dev@live.com>
2021-10-06 10:24:03 -0300

    Add PHP solution to the first question

---
+++ solutions/CountSpecificElementsInArray-1.php
@@ -0,0 +1,21 @@
+<?php
+
+// O(n^2) time | O(1) space, n = number of elements in the input array
+function countSpecificElements(array $input): int {
+    $numberOfElements = 0;
+    for ($i = 0; $i < count($input); $i++) {
+        for ($j = 0; $j < count($input); $j++) {
+            if ($i === $j) {
+                continue;
+            }
+            if ($input[$j] === $input[$i] + 1) {
+                $numberOfElements++;
+                continue 2;
+            }
+        }
+    }
+
+    return $numberOfElements;
+}
+
+include 'tests/CountSpecificElementsInArray.php';
---
+++ solutions/tests/CountSpecificElementsInArray.php
@@ -0,0 +1,18 @@
+<?php
+
+$testCases = [
+    [[1, 2, 3], 2],
+    [[1, 1, 3, 3, 5, 5, 7, 7], 0],
+    [[1, 3, 2, 3, 5, 0], 3],
+];
+
+foreach ($testCases as $k => $testCase) {
+    echo "---------- Test Case $k ----------\n";
+    [$input, $expected] = $testCase;
+    $result = countSpecificElements($input);
+    if ($expected === $result) {
+        echo "OK\n";
+    } else {
+        echo sprintf("Failed: Expected %s but got %s instead\n", $expected, $result);
+    }
+}
